1a21ed3d4474e42ee76e38d2e295039ee1fb3c50
Crowdbotics <team@crowdbotics.com>
2024-01-29 04:39:10 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "buffalo-wingsworldc-45648",
+  url: "https://buffalo-wingsworldc-45648.botics.co",
+  api: "https://buffalo-wingsworldc-45648.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
